25f5c004892448b261cb94f0356e963c863662d3
Amardeep Mann <amar2slick@gmail.com>
2023-03-14 22:04:30 -0700

    Set favicon and got peer review

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
-import logo from './logo.svg';
+// Amardeep Mann
+// 03-14-23
+// Advice Generator App
+// We made a react app for an advice generator app that uses an app to print out new advice
+// Peer Reviewed By: Pedro Casteneda
+// 
+
 import './App.css';
 import AdviceCard from './components/AdviceCard';
 
